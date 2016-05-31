334e7ce3299485cc7b64eee79cdff9c65e91523c
Luke Wilde <luke.wilde@gmail.com>
2016-05-31 22:45:57 +0100

    Show hide graph button.

--- src/js/game/lib/reporting.js
+++ src/js/game/lib/reporting.js
@@ -19,10 +19,26 @@ module.exports = {
       }
     };
 
-    var chartDiv = document.getElementById('chart-div');
-    chartDiv.style.display = 'block';
+    var chartDiv = document.getElementById('chart');
+    var chartContainer = document.getElementById('chart-container');
+
 
     var chart = new window.google.visualization.ScatterChart(chartDiv);
+
+    var showButton = document.getElementsByClassName('show-button');
+
+    chartContainer.style.display = 'block';
+    var graphOpen = true;
+
+    showButton[0].addEventListener('click', function() {
+      if (graphOpen) {
+        chartContainer.style.left = '-800px';
+      } else {
+        chartContainer.style.left = '0px';
+      }
+
+      graphOpen = !graphOpen;
+    }, false);
 
     chart.draw(data, options);
   }
