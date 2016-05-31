e5bd0ca20af87ee47f844367992397a95b1d00af
Luke Wilde <luke.wilde@gmail.com>
2016-05-31 22:30:02 +0100

    Adding cheeky state machine.

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -6,46 +6,65 @@ var maxGenerations = 100;
 var currentGeneration = 0;
 
 var fittest = null;
-var fittestFromGeneration = null;
-var reported = false;
+var networkToRender = null;
+
+var EVOLVING = 0;
+var REPORTING = 1;
+var DONE = 2;
+
+var currentState = EVOLVING;
 
 game.create = function () {
   fittest = evolution.createInitialPopulation(populationSize, game);
 };
 
 game.update = function() {
-
-  var done = currentGeneration >= maxGenerations;
-
-  if (done && !reported) {
-    fittest.reportFitness();
-    console.log('Winner: %s from generation', fittest.fitness, fittest.generation);
-    reported = true;
-    fittestFromGeneration = fittest;
-    reporting.showGraph(evolution.chartData, maxGenerations);
-    return;
-  } else if (done) {
-    return;
+  switch (currentState) {
+  case EVOLVING:
+    createNextGeneration();
+    break;
+  case REPORTING:
+    report();
+    break;
+  case DONE:
+    break;
+  default:
+    console.warn('In unknown state %s', currentState);
   }
-
-  if (!done) {
-
-    fittestFromGeneration = evolution.nextGeneration(currentGeneration);
-    currentGeneration ++;
-
-    console.log('Generation %s: %s', currentGeneration, fittestFromGeneration.fitness);
-
-    if (fittest.fitness > fittestFromGeneration.fitness) {
-      fittest = fittestFromGeneration;
-    }
-  }
-
 };
 
 game.render = function() {
   // Destroy all old graphics.
   game.world.removeAll();
-  fittestFromGeneration.draw();
+  networkToRender.draw();
 };
+
+function createNextGeneration() {
+  networkToRender = evolution.nextGeneration(currentGeneration);
+
+  console.log('Generation %s: %s', currentGeneration, networkToRender.fitness);
+
+  if (fittest.fitness > networkToRender.fitness) {
+    fittest = networkToRender;
+  }
+
+  checkIfMaxGenerations();
+}
+
+function checkIfMaxGenerations() {
+  currentGeneration ++;
+
+  if (currentGeneration >= maxGenerations) {
+    currentState = REPORTING;
+  }
+}
+
+function report() {
+  fittest.reportFitness();
+  console.log('Winner: %s from generation %s', fittest.fitness, fittest.generation);
+  networkToRender = fittest;
+  reporting.showGraph(evolution.chartData, maxGenerations);
+  currentState = DONE;
+}
 
 module.exports = game;
