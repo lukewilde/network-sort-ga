9933d5719dc264c0027e0ddf59c18beb532dcd84
Luke Wilde <luke.wilde@clock.co.uk>
2016-06-27 11:27:37 +0200

    Prepairing to spawn packing populatino from peers.

--- src/js/game/lib/mating-bucket.js
+++ src/js/game/lib/mating-bucket.js
@@ -6,18 +6,26 @@ module.exports = {
 
   population: null,
 
-  populate: function(population) {
+  populate: function(population, length) {
 
     this.population = population;
 
     _.each(population, _.bind(function(item, index) {
 
-      var bucketSlots = Math.round(population.length / (index + 2));
+      var bucketSlots = Math.round(length / (index + 2));
 
       _.times(bucketSlots, _.bind(function() {
         this.bucket.push(index);
       }, this));
     }, this));
+  },
+
+  spawnFromPopulation: function(population) {
+    this.population(population, population.length);
+  },
+
+  spawnFromSelection: function(population, length) {
+    this.populate(population, length);
   },
 
   getMate: function() {
@@ -26,7 +34,7 @@ module.exports = {
   },
 
   getNewPopulation: function(population) {
-    this.populate(population);
+    this.spawnFromPopulation(population);
     var newPopulation = [];
 
     _.each(this.population, _.bind(function() {
